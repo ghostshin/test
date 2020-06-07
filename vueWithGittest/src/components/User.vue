358f435b925d9e484e5bcbbc6a0004a100bf84b9
xushen <xlelouchshin@163.com>
2020-06-07 21:17:13 +0800

    动态路由。含参数。

--- vueWithGittest/src/components/User.vue
+++ vueWithGittest/src/components/User.vue
@@ -1,8 +1,19 @@
 <template>
-  <h2>我是用户页面</h2>
+  <div>
+    <h2>我是用户页面</h2>
+    <p>{{userId}}</p>
+  </div>
 </template>
 
 <script>
+  export default {
+    name: "User",
+    computed: {
+      userId() {
+        return this.$route.params.userId
+      }
+    }
+  }
 </script>
 
 <style>
